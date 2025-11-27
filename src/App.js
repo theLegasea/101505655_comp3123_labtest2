a24dcd2a07d2466e4bcc8a8566ea08641f4291b4
Riley <rileythomas.elevate@gmail.com>
2025-11-27 12:34:49 -0500

    set up states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const  App = () => {
+
+    const[city,setCity] = useState("");
+    const[weather,setWeather] = useState(null);
+    const[loading,setLoading] = useState(false);
+    const[error,setError] = useState(null);
+    const apiKey = "20fd0bd4e1e3767428c9f326c6470f97";
+
   return (
     <div className="App">
       <header className="App-header">
