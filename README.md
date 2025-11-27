56824787e085e04387227e439b6b95b75c8ff2b2
Riley <rileythomas.elevate@gmail.com>
2025-11-27 12:53:22 -0500

    scaffold in the app card, establish todos, add bootstrap

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Project Setup
+# Project Log
 ***
 *1. Initially fumble and cause a commit unattached to the head, reclone the repository*  
 *2. Create an account and get an API key form openweathermap*  
 *3. Initialize the react app with npx create-react-app*  
+*4. Scaffold in the initial card for the app*
+*5. Author the weather fetch function*
 ***
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const  App = () => {
 
@@ -10,22 +11,23 @@ const  App = () => {
     const[error,setError] = useState(null);
     const apiKey = "20fd0bd4e1e3767428c9f326c6470f97";
 
+    /*TODO: fetch weather function*/
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+        <div className="card col-md mx-auto">
+            <div className="card-body">
+                <h2 className="text-center mb-3">Weather Tracking</h2>
+
+                <input/>
+                <button>Get Weather Updates</button>
+
+            {/*TODO: if weather fetched - content,
+                     while weather loads - spinner?,
+                     if weather fails to fetch*/}
+            </div>
+        </div>
     </div>
   );
 }
